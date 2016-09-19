a88dddd658cfd411816323b0f5777eeea5394188
chatkup K <chatkup@gmail.com>
2016-09-19 22:41:45 +0700

    add provider2

---
+++ app/providers/pro-2/pro-2.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the Pro2 provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class Pro2 {
+
+  constructor(private http: Http) {}
+
+}
+
